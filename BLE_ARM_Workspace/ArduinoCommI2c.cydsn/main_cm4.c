0211f1c9ac9d0c12d4a918c6d055c233aad40d32
Denzerek <4150824@gmail.com>
2021-10-15 13:35:06 +0530

    re

--- BLE_ARM_Workspace/ArduinoCommI2c.cydsn/main_cm4.c
+++ BLE_ARM_Workspace/ArduinoCommI2c.cydsn/main_cm4.c
@@ -31,14 +31,26 @@ int main(void)
     
     cy_stc_scb_i2c_master_xfer_config_t transfer;
     uint8_t readBuffer [5UL];
-    uint8_t writeBuffer[2UL] = {67, 5U};
+    uint8_t writeBuffer[2UL] = {0xCD, 5U};
     /* Configure write transaction */
     transfer.slaveAddress = 0x08U;
     transfer.buffer       = writeBuffer;
     transfer.bufferSize   = sizeof(writeBuffer);
-    //transfer.xferPending  = true; /* Do not generate Stop condition at the end of transaction */
-    transfer.xferPending  = false; /* Generate Stop condition at the end of transaction */
+    transfer.xferPending  = true; /* Do not generate Stop condition at the end of transaction */
+    //transfer.xferPending  = false; /* Generate Stop condition at the end of transaction */
     i2cStatus = 0;
+    /* Initiate write transaction.
+    * The Start condition is generated to begin this transaction.
+    */
+    (void) Cy_SCB_I2C_MasterWrite(I2C_1_HW, &transfer, &I2C_1_context);
+    /* Wait for transaction completion */
+    while (0UL != (CY_SCB_I2C_MASTER_BUSY & Cy_SCB_I2C_MasterGetStatus(I2C_1_HW, &I2C_1_context)))
+    { 
+    }
+    writeBuffer[0] = 0xAC;
+     transfer.buffer       = writeBuffer;
+    transfer.xferPending  = false; /* Generate Stop condition at the end of transaction */
+    
     /* Initiate write transaction.
     * The Start condition is generated to begin this transaction.
     */
@@ -49,19 +61,6 @@ int main(void)
     }
     printf("I2c master write operation complete %X\r\n",i2cStatus);
     
-    /* Configure read transaction */
-    transfer.buffer       = readBuffer;
-    transfer.bufferSize   = sizeof(readBuffer);
-    transfer.xferPending  = false; /* Generate Stop condition the end of transaction */
-    /* Initiate read transaction.
-    * The ReStart condition is generated to begin this transaction because
-    * previous transaction was completed without Stop.
-    */
-    (void) Cy_SCB_I2C_MasterRead(I2C_1_HW, &transfer, &I2C_1_context);
-    /* Wait for transaction completion */
-    while (0UL != (CY_SCB_I2C_MASTER_BUSY & Cy_SCB_I2C_MasterGetStatus(I2C_1_HW, &I2C_1_context)))
-    {
-    }
     
     
     
