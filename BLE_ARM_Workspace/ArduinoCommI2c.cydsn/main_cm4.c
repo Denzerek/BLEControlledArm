a5058ec7e8808be16ad811c1f6a890f83a4b265c
Denzerek <4150824@gmail.com>
2021-10-15 14:42:19 +0530

    re

--- BLE_ARM_Workspace/ArduinoCommI2c.cydsn/main_cm4.c
+++ BLE_ARM_Workspace/ArduinoCommI2c.cydsn/main_cm4.c
@@ -31,13 +31,16 @@ int main(void)
     
     cy_stc_scb_i2c_master_xfer_config_t transfer;
     uint8_t readBuffer [5UL];
-    uint8_t writeBuffer[10UL] = {'A','B','c','d','e', 5U};
+    uint8_t writeBuffer[10UL];
+    strcpy(writeBuffer,"cmd ");
+    writeBuffer[4] = 1;
+    writeBuffer[5] = 0xEE;
     /* Configure write transaction */
     transfer.slaveAddress = 0x08U;
     transfer.buffer       = writeBuffer;
     transfer.bufferSize   = sizeof(writeBuffer);
-    transfer.xferPending  = true; /* Do not generate Stop condition at the end of transaction */
-    //transfer.xferPending  = false; /* Generate Stop condition at the end of transaction */
+    //transfer.xferPending  = true; /* Do not generate Stop condition at the end of transaction */
+    transfer.xferPending  = false; /* Generate Stop condition at the end of transaction */
     i2cStatus = 0;
     /* Initiate write transaction.
     * The Start condition is generated to begin this transaction.
@@ -47,6 +50,8 @@ int main(void)
     while (0UL != (CY_SCB_I2C_MASTER_BUSY & Cy_SCB_I2C_MasterGetStatus(I2C_1_HW, &I2C_1_context)))
     { 
     }
+    
+    #if 0
     writeBuffer[0] = 0xA;
      transfer.buffer       = writeBuffer;
     transfer.xferPending  = false; /* Generate Stop condition at the end of transaction */
@@ -59,6 +64,7 @@ int main(void)
     while (0UL != (CY_SCB_I2C_MASTER_BUSY & Cy_SCB_I2C_MasterGetStatus(I2C_1_HW, &I2C_1_context)))
     { 
     }
+    #endif
     printf("I2c master write operation complete %X\r\n",i2cStatus);
     
     
