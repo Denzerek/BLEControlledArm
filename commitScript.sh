7e8fe2c13b98f04fdeaa9edbce4bef1bb1a12239
Denzerek <4150824@gmail.com>
2021-10-19 17:35:17 +0530

    re

--- commitScript.sh
+++ commitScript.sh
@@ -10,4 +10,4 @@ eval "$(ssh-agent -s)"
 ssh-add /c/Users/Austina/.ssh/Denzerek_key
 
 
-git add . && git commit -m "$test" && git push -u origin main
+git add . && git commit -m "$test" && git push -u origin Branch1
