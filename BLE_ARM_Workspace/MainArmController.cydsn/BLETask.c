94a039df462aff350371c8d6005d6409cd3ada5a
Denzerek <4150824@gmail.com>
2021-10-24 18:16:31 +0530

    re

--- BLE_ARM_Workspace/MainArmController.cydsn/BLETask.c
+++ BLE_ARM_Workspace/MainArmController.cydsn/BLETask.c
@@ -112,9 +112,9 @@ void genericEventHandler(uint32_t event,void* eventParam)
         case CY_BLE_EVT_GATTS_WRITE_REQ:
         writeRequestParam = (cy_stc_ble_gatts_write_cmd_req_param_t*)eventParam;
         
-    ble_printf("Received Write from Handle :%d",writeRequestParam->handleValPair.attrHandle);
         for(int i = 0;i < M_MAX ; i++)
         {
+    ble_printf("Handle :%X    CHandle:%X",writeRequestParam->handleValPair.attrHandle,motorToControl[i].BLECharacteristicHandle);
             if(motorToControl[i].BLECharacteristicHandle == writeRequestParam->handleValPair.attrHandle)
                 motorToControl[i].callBackfn(motorToControl[i].motor,writeRequestParam->handleValPair.value.val[0],CY_BLE_GATT_DB_PEER_INITIATED);
             
