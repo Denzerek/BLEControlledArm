d04fc8a779605256bfc9cb175625697caa45d2f7
Denzerek <4150824@gmail.com>
2021-10-25 10:09:42 +0530

    Updated CapsenseBLECentral

--- BLE_ARM_Workspace/CapsenseBLECentral.cydsn/capSenseTask.c
+++ BLE_ARM_Workspace/CapsenseBLECentral.cydsn/capSenseTask.c
@@ -17,6 +17,78 @@ void capsenseTask(void *arg)
 {
     (void) arg;
     
+    motors_t currentMotor = M1;
+    static uint8_t pressReleased = true;
+    
+    
+    
+    CapSense_Start();
+    CapSense_ScanAllWidgets();
+    
+    cap_print("Capsense Task Started")
+    
+    for(;;)
+    {
+        if(!CapSense_IsBusy())
+        {
+            CapSense_ProcessAllWidgets();
+            int pos;
+            pos = CapSense_GetCentroidPos(CapSense_LINEARSLIDER0_WDGT_ID);
+            
+            if(pos < 0xFFFF)
+            {
+                writeMotorPosition(currentMotor,POS_ABSOLUTE,pos);
+            }
+            
+            if(CapSense_IsWidgetActive(CapSense_BUTTON0_WDGT_ID))
+            {
+                if(pressReleased)
+                {
+                    if(currentMotor < M_MAX - 1)
+                    {
+                        currentMotor++;
+                        pressReleased = false;
+                        cap_printf("Motor M%d selected",currentMotor);
+                    }
+                }
+            }
+            else if(CapSense_IsWidgetActive(CapSense_BUTTON1_WDGT_ID))
+            {
+                if(pressReleased)
+                {
+                    if(currentMotor > M1)
+                    {
+                        currentMotor--;
+                        pressReleased = false;
+                        cap_printf("Motor M%d selected",currentMotor);
+                    }
+                }
+            }
+            else
+            {
+                pressReleased = true;
+            }
+            
+            if(currentMotor >= M_MAX) currentMotor = M_MAX - 1;
+            if(currentMotor < M1) currentMotor = M1;
+            
+            CapSense_UpdateAllBaselines();
+            CapSense_ScanAllWidgets();
+        }
+        else
+        {
+            vTaskDelay(50);
+        }
+    }
+}
+#if 0
+#include "capSenseTask.h"
+#include "BLETask.h"
+
+void capsenseTask(void *arg)
+{
+    (void) arg;
+    
     motors_t currentMotor = M1;  
     
     CapSense_Start();
@@ -57,4 +129,5 @@ void capsenseTask(void *arg)
         }
     }
 }
+#endif
 /* [] END OF FILE */
